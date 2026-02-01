2302cdb6c9aaa196e743cb827caed4ef9c03e892
Ruchi Kumari <ruchiar197@gmail.com>
2026-02-01 11:48:51 +0530

    Fix wildcard route with regex pattern

--- server/server.js
+++ server/server.js
@@ -55,7 +55,7 @@ app.use('/api', apiRoutes);
 // Production Setup
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
-    app.get('/*', (req, res) => {
+    app.get(/.*/, (req, res) => {
         res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
     });
 }
